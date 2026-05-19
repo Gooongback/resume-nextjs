635b7091ee2a25c178cf916ec753aa7955b77809
Hdy <gongbackgi@gmail.com>
2026-05-19 19:24:22 +0900

    기능: 프로필 및 타이틀 작업 새 브랜치에 저장

--- payload/profile.ts
+++ payload/profile.ts
@@ -11,13 +11,13 @@ const profile: IProfile.Payload = {
   // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
   image,
   name: {
-    title: 'Tux Linux Penguin',
-    small: '(Tux)',
+    title: '황대연',
+    small: '반갑습니다',
   },
   contact: [
     {
-      title: 'tux@linux.org',
-      link: 'mailto:tux@linux.org',
+      title: '2288003@pcu.ac.kr',
+      link: '2288003@pcu.ac.kr',
       icon: faEnvelope,
     },
     {
@@ -26,17 +26,17 @@ const profile: IProfile.Payload = {
       badge: true,
     },
     {
-      link: 'https://github.com/uyu423/resume-nextjs',
+      link: 'https://github.com/Gooongback',
       icon: faGithub,
     },
     {
-      link: 'https://www.facebook.com/groups/ubuntu.ko',
+      link: 'non facebook',
       icon: faFacebook,
     },
   ],
   notice: {
     title:
-      "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
+      "포트폴리오용 웹 이력서입니다.",
     icon: faBell,
   },
 };
